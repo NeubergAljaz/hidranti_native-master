4627e1f627804ec72aed9e91d598e4ef7825240c
Aljaz <aljaz.neuberg@student.um.si>
2023-05-24 14:19:21 +0200

    Popravljena kamera

--- src/components/Camera/CameraComponent.tsx
+++ src/components/Camera/CameraComponent.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Button, Image, Text, View, Platform } from 'react-native';
-import { Camera as ExpoCamera, CameraType } from 'expo-camera';
+import { Button, Image, Text, View, Platform, TouchableOpacity, StyleSheet  } from 'react-native';
+import { Camera as ExpoCamera, CameraType, FlashMode } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import api from '../../services/api';
 import { BASE_URL_HIDRANT_SLIKA } from '../../config';
-import * as FileSystem from 'expo-file-system';
-import axios from 'axios';
+import { Ionicons } from '@expo/vector-icons';
 import mime from 'mime';
 
 export const CameraComponent: React.FC = () => {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [type] = useState(CameraType.back);
   const [image, setImage] = useState<string | null>(null);
+  const [flash, setFlash] = useState(FlashMode.off);
   const cameraRef = useRef<ExpoCamera | null>(null);
 
   const getPermissions = async () => {
@@ -77,16 +77,73 @@ export const CameraComponent: React.FC = () => {
     }
   }
 
+  const toggleFlash = () => {
+    setFlash(flash === FlashMode.off ? FlashMode.on : FlashMode.off);
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <ExpoCamera style={{ flex: 1 }} type={type} ref={cameraRef}>
-        <View>
-          <Button title="Slikaj" onPress={takePicture} />
-          <Button title="Izberi iz galerije" onPress={pickImage} />
-          {image && <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />}
-          <Button title="Poslji" onPress={handleSubmit} />
+    <ExpoCamera style={{ flex: 1 }} type={type} flashMode={flash} ref={cameraRef}>
+      <View style={styles.container}>
+        {image && <Image source={{ uri: image }} style={styles.imagePreview} />}
+
+        <TouchableOpacity style={styles.flashButton} onPress={toggleFlash}>
+          <Ionicons name={flash === FlashMode.off ? "flash-off-outline" : "flash-outline"} size={30} color="#fff" />
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+          <Ionicons name="camera-outline" size={40} color="#fff" />
+        </TouchableOpacity>
+
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity style={styles.iconButton} onPress={pickImage}>
+            <Ionicons name="images-outline" size={30} color="#fff" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.iconButton} onPress={handleSubmit}>
+            <Ionicons name="checkmark-circle-outline" size={30} color="#fff" />
+          </TouchableOpacity>
         </View>
-      </ExpoCamera>
-    </View>
+      </View>
+    </ExpoCamera>
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  captureButton: {
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 50,
+    padding: 20,
+    paddingHorizontal: 25,
+    alignSelf: 'center',
+    margin: 20,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  imagePreview: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 100, 
+    height: 100
+  },
+  flashButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+});
